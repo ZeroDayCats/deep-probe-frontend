4030b2b83f9a0082d64e6adcd186ee877b5f9cba
danish-mar <danishs2007@gmail.com>
2025-09-20 12:53:38 +0530

    fix: fixed allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default ({ mode }: { mode: string }) => {  // Load .env variables based on current mode (development/production)
+  
+  const env = loadEnv(mode, process.cwd(), "");
 
+  // Convert comma-separated string to array
   const allowedHosts = (env.ALLOWED_HOSTS || "").split(",").map(h => h.trim());
 
-  server: {
-    host: "0.0.0.0",
-    allowedHosts
-  }
-})
+  console.log("Allowed Hosts ");
+  console.log(allowedHosts);
+    
+
+
+  return defineConfig({
+    plugins: [react()],    
+    server: {
+      host: "0.0.0.0",
+      allowedHosts,
+    },
+  });
+};
