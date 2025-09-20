447b87128b680f847e91a6044197751fb8cfb1cf
danish-mar <danishs2007@gmail.com>
2025-09-20 12:49:07 +0530

    fix: cors and optimized backend

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  const allowedHosts = (env.ALLOWED_HOSTS || "").split(",").map(h => h.trim());
+
+  server: {
+    host: "0.0.0.0",
+    allowedHosts
+  }
 })
